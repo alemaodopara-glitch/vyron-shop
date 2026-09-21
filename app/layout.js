eae007cd831e77656caa8e918f540c4cb57e821c
alemaodopara-glitch <alemaodopara@gmail.com>
2026-09-21 16:26:18 -0300

    Build VYRON Shop modern marketplace

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"VYRON | Marketplace",description:"Boas escolhas. Sem complicação."};
+export default function Layout({children}){return <html lang="pt-BR"><body>{children}</body></html>}
